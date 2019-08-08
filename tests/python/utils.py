95aa776b33867528c81fc0b6de577fffe6888db3
Elizaveta Dokshina <sabitova_liza@icloud.com>
2019-08-08 12:31:18 +0000

    RPM Dependency with Tests
    Added append_tarantool_dependency_tags function to append tarantool
    dependency tags to rpm header in case of usinr Tarantool Opensource
    Added tests: rpmfile module is used to check if rpm header has right
    dependency tags

--- tests/python/utils.py
+++ tests/python/utils.py
@@ -5,9 +5,14 @@ import subprocess
 
 __tarantool_version = None
 
-def tarantool_enterprise_is_used():
+def tarantool_version():
     global __tarantool_version
     if __tarantool_version is None:
-        __tarantool_version = subprocess.check_output(['tarantool', '-V']).decode('ascii')
+        __tarantool_version = subprocess.check_output(['tarantool', '-V']).decode('ascii').split('\n')[0]
 
-    return __tarantool_version.startswith('Tarantool Enterprise')
+    return __tarantool_version
+
+
+def tarantool_enterprise_is_used():
+    return tarantool_version().startswith('Tarantool Enterprise')
+
