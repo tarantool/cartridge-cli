407865557acda5010038d78aedf94f872d7b3451
Max Melentiev <m.melentiev@corp.mail.ru>
2019-08-29 19:44:48 +0300

    Emulate NOTIFY_SOKET in old tarantool

--- test/instances/simple.lua
+++ test/instances/simple.lua
@@ -4,3 +4,16 @@ local fiber = require('fiber')
 fiber.create(function()
     fiber.sleep(1)
 end)
+
+-- Copied from cartridge.cfg to provide support for NOTIFY_SOCKET in old tarantool
+local tnt_version = _TARANTOOL:split('.')
+local tnt_major = tonumber(tnt_version[1])
+local tnt_minor = tonumber(tnt_version[2])
+if tnt_major < 2 or (tnt_major == 2 and tnt_minor < 2) then
+  local notify_socket = os.getenv('NOTIFY_SOCKET')
+  if notify_socket then
+      local socket = require('socket')
+      local sock = assert(socket('AF_UNIX', 'SOCK_DGRAM', 0), 'Can not create socket')
+      sock:sendto('unix/', notify_socket, 'READY=1')
+  end
+end
