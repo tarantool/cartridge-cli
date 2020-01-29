b7649ed2d0b4d9996e4280a752d40247fd4f2c8c
Max Melentiev <m.melentiev@corp.mail.ru>
2020-01-29 17:58:21 +0300

    Add luacov to tests and template

--- templates/cartridge/deps.sh
+++ templates/cartridge/deps.sh
@@ -4,4 +4,7 @@
 set -e
 
 tarantoolctl rocks make
+# Dev and test dependencies:
+tarantoolctl rocks install luatest 0.5.0
+tarantoolctl rocks install luacov 0.13.0
 tarantoolctl rocks install luacheck 0.25.0
