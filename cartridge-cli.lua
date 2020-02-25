d02c51bef3ce450fc374ad0d2d0ddd6a4d722711
Elizaveta Dokshina <32142520+dokshina@users.noreply.github.com>
2020-02-25 15:18:01 +0300

    Use git-core instead of git in base image (#157)
    * base image size: 573MB(git) -> 510MB(git-core)
    * download size:    73 M(git)     -> 56 M(git-core)
    * installed size:     222 M(git)   -> 69 M(git-core)

--- cartridge-cli.lua
+++ cartridge-cli.lua
@@ -1291,7 +1291,7 @@ local DOCKERFILE_PREPARE = [[
 ### Prepare
 SHELL ["/bin/bash", "-c"]
 
-RUN yum install -y git gcc make cmake unzip
+RUN yum install -y git-core gcc make cmake unzip
 
 # create user and directories
 RUN groupadd -r tarantool \
