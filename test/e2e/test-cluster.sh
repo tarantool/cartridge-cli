8f0da3627af07847755f90e4322fa1d4566d664f
Elizaveta Dokshina <32142520+dokshina@users.noreply.github.com>
2020-04-21 16:25:43 +0300

    Test app stateboard on e2e tests (#223)

--- test/e2e/test-cluster.sh
+++ test/e2e/test-cluster.sh
@@ -9,7 +9,7 @@ curl -w "\n" -X POST http://127.0.0.1:8081/admin/api --fail -d@- <<QUERY
     "mutation {
         j1: join_server(
             uri:\"$IP:3301\",
-            roles: [\"vshard-router\", \"app.roles.custom\"]
+            roles: [\"vshard-router\", \"app.roles.custom\", \"failover-coordinator\"]
             instance_uuid: \"aaaaaaaa-aaaa-4000-b000-000000000001\"
             replicaset_uuid: \"aaaaaaaa-0000-4000-b000-000000000000\"
         )
@@ -21,6 +21,18 @@ curl -w "\n" -X POST http://127.0.0.1:8081/admin/api --fail -d@- <<QUERY
             timeout: 5
         )
         bootstrap_vshard
+        enable_failover: cluster{
+            failover_params(
+                mode: \"stateful\",
+                tarantool_params: {
+                    uri: \"$IP:3310\", password: \"passwd\"
+                },
+                state_provider: \"tarantool\"
+            ) {
+                mode
+                state_provider
+            }
+        }
     }"
 }
 QUERY
@@ -41,8 +53,16 @@ tarantoolctl connect /var/run/tarantool/myapp.instance_1.control <<COMMAND
     assert(s1.alias == 'i1', 'Invalid i1 alias')
     assert(s2.alias == 'i2', 'Invalid i2 alias')
 
-    assert(s1.replicaset.roles[1] == 'vshard-router', 'Missing s1 router role')
+    assert(s1.replicaset.roles[1] == 'failover-coordinator', 'Missing s1 failover coordinator role')
+    assert(s1.replicaset.roles[2] == 'vshard-router', 'Missing s1 router role')
+    assert(s1.replicaset.roles[3] == 'app.roles.custom', 'Missing s1 custom role')
+
     assert(s2.replicaset.roles[1] == 'vshard-storage', 'Missing s2 storage role')
-    assert(s1.replicaset.roles[2] == 'app.roles.custom', 'Missing s1 custom role')
+
+    local failover_params = cartridge.failover_get_params()
+    log.info('%s', yaml.encode(failover_params))
+
+    assert(failover_params.mode == 'stateful', 'Failover mode should be stateful')
+    assert(failover_params.state_provider == 'tarantool', 'Failover state provider should be tarantool')
 COMMAND"
 echo " - Cluster is ready"
