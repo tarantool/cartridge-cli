14ff306a3d833f7b3adfc079d1aecc7601093df0
Elizaveta Dokshina <32142520+dokshina@users.noreply.github.com>
2020-05-20 11:42:43 +0300

    Add a spinner on quiet run (#246)

--- common/execute.go
+++ common/execute.go
@@ -6,19 +6,73 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sync"
+	"time"
+
+	"github.com/briandowns/spinner"
 )
+
+var (
+	spinnerPicture    = spinner.CharSets[9]
+	spinnerUpdateTime = 100 * time.Millisecond
+)
+
+const (
+	ready = 1
+)
+
+// startAndWaitCommand executes command
+// and sends `ready` flag to the channel before return
+func startAndWaitCommand(cmd *exec.Cmd, c chan int, wg *sync.WaitGroup, err *error) {
+	defer wg.Done()
+	defer func() { c <- ready }() // say that command is complete
+
+	if *err = cmd.Start(); *err != nil {
+		return
+	}
+
+	if *err = cmd.Wait(); *err != nil {
+		return
+	}
+}
+
+// startCommandSpinner starts running spinner
+// until `ready` flag is received from the channel
+func startCommandSpinner(c chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	s := spinner.New(spinnerPicture, spinnerUpdateTime)
+	s.Start()
+
+	// wait for the command to complete
+	_ = <-c
+
+	s.Stop()
+}
 
 // RunCommand runs specified command and returns an error
 // If showOutput is set to true, command output is shown
+// Else spinner is shown while command is running
 func RunCommand(cmd *exec.Cmd, dir string, showOutput bool) error {
-	cmd.Dir = dir
+	var err error
+	var wg sync.WaitGroup
+	c := make(chan int, 1)
 
+	cmd.Dir = dir
 	if showOutput {
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
+	} else {
+		wg.Add(1)
+		go startCommandSpinner(c, &wg)
 	}
 
-	return cmd.Run()
+	wg.Add(1)
+	go startAndWaitCommand(cmd, c, &wg, &err)
+
+	wg.Wait()
+
+	return err
 }
 
 // RunHook runs specified hook and returns an error
