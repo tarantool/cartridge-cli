19b4d7e4132c298049f721a3720078141d37a256
Elizaveta Dokshina <eldokshina@mail.ru>
2020-06-25 20:27:46 +0300

    Update cartridge to 2.2.0

--- cartridge-cli/templates/cartridge/files/app_files.lua
+++ cartridge-cli/templates/cartridge/files/app_files.lua
@@ -13,7 +13,7 @@ local app_files = {
                 'tarantool',
                 'lua >= 5.1',
                 'checks == 3.0.1-1',
-                'cartridge == 2.1.2-1',
+                'cartridge == 2.2.0-1',
                 'metrics == 0.3.0-1',
             }
             build = {
