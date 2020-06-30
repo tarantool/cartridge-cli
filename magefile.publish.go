e8b7921f72debbf3c2f822411da163d3265a035a
Elizaveta Dokshina <eldokshina@mail.ru>
2020-06-30 14:29:01 +0300

    Publish to Packagecloud

--- magefile.publish.go
+++ magefile.publish.go
@@ -14,20 +14,23 @@ import (
 
 const s3UpdateRepoScriptEnv = "S3_UPDATE_REPO_SCRIPT_URL"
 const s3UpdateRepoScriptName = "update_repo.sh"
-
 const s3BucketEnv = "S3_BUCKET"
 const s3FolderEnv = "S3_FOLDER"
+
+const pkgcloudUserEnv = "PACKAGECLOUD_USER"
 
 const distPath = "dist"
 
 const packageName = "cartridge-cli"
+const rpmExt = "rpm"
+const debExt = "deb"
 
-type RepoInfo struct {
+type Distro struct {
 	OS   string
 	Dist string
 }
 
-var targetRepos = []RepoInfo{
+var targetDistros = []Distro{
 	{OS: "el", Dist: "6"},
 	{OS: "el", Dist: "7"},
 	{OS: "el", Dist: "8"},
@@ -41,6 +44,64 @@ var targetRepos = []RepoInfo{
 	{OS: "debian", Dist: "jessie"},
 	{OS: "debian", Dist: "stretch"},
 	{OS: "debian", Dist: "buster"},
+}
+
+var pkgcloudRepos = []string{
+	"1_10",
+	"2x",
+	"2_2",
+	"2_3",
+	"2_4",
+}
+
+func getArch(distro Distro) (string, error) {
+	if distro.OS == "el" || distro.OS == "fedora" {
+		return "x86_64", nil
+	}
+
+	if distro.OS == "ubuntu" || distro.OS == "debian" {
+		return "amd64", nil
+	}
+
+	return "", fmt.Errorf("Unknown OS: %s", distro.OS)
+}
+
+func getExt(distro Distro) (string, error) {
+	if distro.OS == "el" || distro.OS == "fedora" {
+		return rpmExt, nil
+	}
+
+	if distro.OS == "ubuntu" || distro.OS == "debian" {
+		return debExt, nil
+	}
+
+	return "", fmt.Errorf("Unknown OS: %s", distro.OS)
+}
+
+func getPackagePath(distro Distro) (string, error) {
+	ext, err := getExt(distro)
+	if err != nil {
+		return "", fmt.Errorf("Failed to get ext: %s", err)
+	}
+
+	arch, err := getArch(distro)
+	if err != nil {
+		return "", fmt.Errorf("Failed to get arch: %s", err)
+	}
+
+	packageNamePattern := fmt.Sprintf("%s-*.%s.%s", packageName, arch, ext)
+	matches, err := filepath.Glob(filepath.Join(distPath, packageNamePattern))
+	if err != nil {
+		return "", fmt.Errorf("Failed to find matched files: %s", err)
+	}
+
+	if len(matches) == 0 {
+		return "", fmt.Errorf("No matched packages found")
+	} else if len(matches) > 1 {
+		return "", fmt.Errorf("Found multiple matched packages: %v", matches)
+	}
+
+	return matches[0], nil
 }
 
 func getS3Ctx() (map[string]string, error) {
@@ -91,13 +152,13 @@ func PublishS3() error {
 		return err
 	}
 
-	for _, targetRepo := range targetRepos {
-		fmt.Printf("Publish package for %s/%s...\n", targetRepo.OS, targetRepo.Dist)
+	for _, targetDistro := range targetDistros {
+		fmt.Printf("Publish package for %s/%s...\n", targetDistro.OS, targetDistro.Dist)
 
 		err := sh.RunV(
 			"bash", publishCtx["s3UpdateRepoScriptPath"],
-			fmt.Sprintf("-o=%s", targetRepo.OS),
-			fmt.Sprintf("-d=%s", targetRepo.Dist),
+			fmt.Sprintf("-o=%s", targetDistro.OS),
+			fmt.Sprintf("-d=%s", targetDistro.Dist),
 			fmt.Sprintf("-p=%s", packageName),
 			fmt.Sprintf("-b=%s", publishCtx["s3RepoPath"]),
 			fmt.Sprintf("-f"),
@@ -105,7 +166,40 @@ func PublishS3() error {
 		)
 
 		if err != nil {
-			return fmt.Errorf("Failed to publish package for %s/%s: %s", targetRepo.OS, targetRepo.Dist, err)
+			return fmt.Errorf("Failed to publish package for %s/%s: %s", targetDistro.OS, targetDistro.Dist, err)
+		}
+	}
+
+	return nil
+}
+
+// publish RPM and DEB packages to Packagecloud
+func PublishPkgcloud() error {
+	fmt.Printf("Publish packages to Packagecloud...\n")
+
+	pkgcloudUser := os.Getenv(pkgcloudUserEnv)
+	if pkgcloudUser == "" {
+		return fmt.Errorf("Please, specify %s", pkgcloudUserEnv)
+	}
+
+	for _, targetDistro := range targetDistros {
+		for _, repo := range pkgcloudRepos {
+			var err error
+
+			packagePath, err := getPackagePath(targetDistro)
+			if err != nil {
+				return fmt.Errorf("Failed to get package path: %s", err)
+			}
+
+			err = sh.RunV(
+				"pkgcloud-push",
+				filepath.Join(pkgcloudUser, repo, targetDistro.OS, targetDistro.Dist),
+				packagePath,
+			)
+
+			if err != nil {
+				return fmt.Errorf("Failed to publish package for %s/%s to %s repo: %s", targetDistro.OS, targetDistro.Dist, repo, err)
+			}
 		}
 	}
 
