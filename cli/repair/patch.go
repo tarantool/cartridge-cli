0de844678e3d95821ffc0e097acc6a1c2a65e7dc
Elizaveta Dokshina <32142520+dokshina@users.noreply.github.com>
2020-08-28 17:33:15 +0300

    Reload: Fix Config Eval (#361)

--- cli/repair/patch.go
+++ cli/repair/patch.go
@@ -101,6 +101,16 @@ func reloadConf(topologyConfPath string, instanceName string, ctx *context.Ctx) 
 		local ClusterwideConfig = require('cartridge.clusterwide-config')
 		local confapplier = require('cartridge.confapplier')
 
+		local cfg, err = ClusterwideConfig.load('{{ .ConfigPath }}')
+		if err ~= nil then
+			return nil, string.format('Failed to load new config: %s', err)
+		end
+
+		local current_uuid = box.info().uuid
+		if cfg:get_readonly().topology.servers[current_uuid] == nil then
+			return false
+		end
+
 		local roles_configured_state = 'RolesConfigured'
 		local connecting_fullmesh_state = 'ConnectingFullmesh'
 
@@ -108,7 +118,7 @@ func reloadConf(topologyConfPath string, instanceName string, ctx *context.Ctx) 
 
 		if state == connecting_fullmesh_state then
 			return nil, string.format(
-				'Failed to desire %s config state. Stuck in %s. ' ..
+				'Failed to reach %s config state. Stuck in %s. ' ..
 					'Call "box.cfg({replication_connect_quorum = 0})" in instance console and try again',
 				roles_configured_state, state
 			)
@@ -116,25 +126,13 @@ func reloadConf(topologyConfPath string, instanceName string, ctx *context.Ctx) 
 
 		if state ~= roles_configured_state then
 			return nil, string.format(
-				'Failed to desire %s config state. Stuck in %s',
+				'Failed to reach %s config state. Stuck in %s',
 				roles_configured_state, state
 			)
 		end
 
-		local cfg, err = ClusterwideConfig.load('{{ .ConfigPath }}')
-		if err ~= nil then
-			return nil, string.format('Failed to load new config: %s', err)
-		end
-
 		cfg:lock()
-
-		local current_uuid = box.info().uuid
-		if cfg:get_readonly().topology.servers[current_uuid] == nil then
-			return false
-		end
-
 		local ok, err = confapplier.apply_config(cfg)
-
 		if err ~= nil then
 			return nil, string.format('Failed to apply new config: %s', err)
 		end
