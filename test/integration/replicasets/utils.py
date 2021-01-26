b134a13434e5bab4853eab3ae4be0808879f2ae5
Elizaveta Dokshina <eldokshina@mail.ru>
2021-01-26 18:22:25 +0300

    Add test for replicasets list with zones

--- test/integration/replicasets/utils.py
+++ test/integration/replicasets/utils.py
@@ -1,3 +1,6 @@
+import requests
+
+
 def get_replicaset_by_alias(replicasets, alias):
     replicaset = None
     for r in replicasets:
@@ -13,3 +16,46 @@ def get_list_from_log_lines(log_lines):
         line.split(maxsplit=1)[1]
         for line in log_lines
     ]
+
+
+def set_instance_zone(admin_api_url, instance_name, zone):
+    # get instance UUID
+    query = '''
+        query {
+            servers: servers {
+                uuid
+                alias
+            }
+        }
+    '''
+
+    r = requests.post(admin_api_url, json={'query': query})
+    assert r.status_code == 200
+    resp = r.json()
+    assert 'data' in resp
+    instances = resp['data']['servers']
+
+    uuid = None
+    for instance in instances:
+        if instance['alias'] == instance_name:
+            uuid = instance['uuid']
+            break
+
+    assert uuid is not None
+
+    query = '''
+        mutation {
+        set_zone: cluster{
+            edit_topology(
+                servers: [{
+                    uuid: "%s",
+                    zone: "%s",
+                }]
+            ) { }
+        }}
+        ''' % (uuid, zone)
+
+    r = requests.post(admin_api_url, json={'query': query})
+    assert r.status_code == 200
+    resp = r.json()
+    assert 'data' in resp
