b91f62df4fa426cad136a9b36d266313791fb829
Alexey Romanov <romanov.alexey2000@gmail.com>
2021-05-24 12:41:00 +0300

    Test fix

--- test/integration/cli/test_version.py
+++ test/integration/cli/test_version.py
@@ -5,4 +5,4 @@ def test_version_command(cartridge_cmd):
     for version_cmd in ["version", "-v", "--version"]:
         rc, output = run_command_and_get_output([cartridge_cmd, version_cmd])
         assert rc == 0
-        assert 'Tarantool Cartridge CLI v2' in output
+        assert 'Tarantool Cartridge CLI\n Version:\t2' in output
