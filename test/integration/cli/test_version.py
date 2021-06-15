6e71fa452d09cb15a279697cfd3b6130d4f3cf48
Alexey Romanov <romanov.alexey2000@gmail.com>
2021-06-15 16:54:45 +0300

    Parametrize tests

--- test/integration/cli/test_version.py
+++ test/integration/cli/test_version.py
@@ -1,17 +1,19 @@
+import pytest
 import subprocess
 from utils import run_command_and_get_output
 from project import remove_project_file
 
 
-def test_version_command(cartridge_cmd):
-    for version_cmd in ["version", "-v", "--version"]:
-        rc, output = run_command_and_get_output([cartridge_cmd, version_cmd])
-        assert rc == 0
-        assert 'Tarantool Cartridge CLI\n Version:\t2' in output
-        assert 'Failed to show Cartridge version: Project path . is not a project' in output
+@pytest.mark.parametrize('version_cmd', ['version', '-v', '--version'])
+def test_version_command(cartridge_cmd, version_cmd):
+    rc, output = run_command_and_get_output([cartridge_cmd, version_cmd])
+    assert rc == 0
+    assert 'Tarantool Cartridge CLI\n Version:\t2' in output
+    assert 'Failed to show Cartridge version: Project path . is not a project' in output
 
 
-def test_version_command_with_project(project_with_cartridge, cartridge_cmd, tmpdir):
+@pytest.mark.parametrize('version_cmd', ['version', '-v', '--version'])
+def test_version_command_with_project(project_with_cartridge, version_cmd, cartridge_cmd, tmpdir):
     project = project_with_cartridge
 
     cmd = [
@@ -23,20 +25,21 @@ def test_version_command_with_project(project_with_cartridge, cartridge_cmd, tmp
     process = subprocess.run(cmd, cwd=tmpdir)
     assert process.returncode == 0
 
-    for version_cmd in ["version", "-v", "--version"]:
-        cmd = [
-            cartridge_cmd, version_cmd,
-            f"--project-path={project.path}"
-        ]
+    cmd = [
+        cartridge_cmd, version_cmd,
+        f"--project-path={project.path}"
+    ]
 
-        rc, output = run_command_and_get_output(cmd)
-        assert rc == 0
+    rc, output = run_command_and_get_output(cmd)
+    assert rc == 0
 
-        assert 'Tarantool Cartridge CLI\n Version:\t2' in output
-        assert 'Tarantool Cartridge\n Version:\t2' in output
+    assert 'Tarantool Cartridge CLI\n Version:\t2' in output
+    assert 'Tarantool Cartridge\n Version:\t2' in output
+    assert 'Rocks' not in output
 
 
-def test_version_command_with_rocks(project_with_cartridge, cartridge_cmd, tmpdir):
+@pytest.mark.parametrize('version_cmd', ['version', '-v', '--version'])
+def test_version_command_with_rocks(project_with_cartridge, version_cmd, cartridge_cmd, tmpdir):
     project = project_with_cartridge
 
     cmd = [
@@ -48,62 +51,61 @@ def test_version_command_with_rocks(project_with_cartridge, cartridge_cmd, tmpdi
     process = subprocess.run(cmd, cwd=tmpdir)
     assert process.returncode == 0
 
-    for version_cmd in ["version", "-v", "--version"]:
-        cmd = [
-            cartridge_cmd,
-            version_cmd, "--rocks",
-            f"--project-path={project.path}"
-        ]
+    cmd = [
+        cartridge_cmd,
+        version_cmd, "--rocks",
+        f"--project-path={project.path}"
+    ]
 
-        rc, output = run_command_and_get_output(cmd)
-        assert rc == 0
-        assert 'Tarantool Cartridge CLI\n Version:\t2' in output
-        assert 'Tarantool Cartridge\n Version:\t2' in output
-        assert 'Rocks' in output
-        assert 'metrics ' in output
-        assert 'checks ' in output
-        assert project.name in output
+    rc, output = run_command_and_get_output(cmd)
+    assert rc == 0
+    assert 'Tarantool Cartridge CLI\n Version:\t2' in output
+    assert 'Tarantool Cartridge\n Version:\t2' in output
+    assert 'Rocks' in output
+    assert 'metrics ' in output
+    assert 'checks ' in output
+    assert project.name in output
 
 
-def test_version_command_invalid_project(project_without_dependencies, cartridge_cmd, tmpdir):
+@pytest.mark.parametrize('version_cmd', ['version', '-v', '--version'])
+def test_version_command_invalid_project(project_without_dependencies, version_cmd, cartridge_cmd, tmpdir):
     project = project_without_dependencies
     remove_project_file(project, f'{project.name}-scm-1.rockspec')
 
-    for version_cmd in ["version", "-v", "--version"]:
-        cmd = [
-            cartridge_cmd,
-            version_cmd, "--rocks",
-            f"--project-path={tmpdir}"
-        ]
+    cmd = [
+        cartridge_cmd,
+        version_cmd, "--rocks",
+        f"--project-path={tmpdir}"
+    ]
 
-        rc, output = run_command_and_get_output(cmd)
-        assert rc == 1
-        assert f'Failed to show Cartridge and other rocks versions: Project path {tmpdir} is not a project' in output
+    rc, output = run_command_and_get_output(cmd)
+    assert rc == 1
+    assert f'Failed to show Cartridge and other rocks versions: Project path {tmpdir} is not a project' in output
 
 
-def test_version_command_nonbuilded_project(project_without_dependencies, cartridge_cmd, tmpdir):
+@pytest.mark.parametrize('version_cmd', ['version', '-v', '--version'])
+def test_version_command_nonbuilded_project(project_without_dependencies, version_cmd, cartridge_cmd, tmpdir):
     project = project_without_dependencies
 
-    for version_cmd in ["version", "-v", "--version"]:
-        cmd = [
-            cartridge_cmd,
-            version_cmd, "--rocks",
-            f"--project-path={project.path}"
-        ]
+    cmd = [
+        cartridge_cmd,
+        version_cmd, "--rocks",
+        f"--project-path={project.path}"
+    ]
 
-        rc, output = run_command_and_get_output(cmd)
-        assert rc == 1
-        assert 'Failed to show Cartridge and other rocks versions: ' \
-            'Are dependencies in .rocks directory correct?' in output
+    rc, output = run_command_and_get_output(cmd)
+    assert rc == 1
+    assert 'Failed to show Cartridge and other rocks versions: ' \
+        'Are dependencies in .rocks directory correct?' in output
 
 
-def test_version_command_invalid_path(cartridge_cmd):
-    for version_cmd in ["version", "-v", "--version"]:
-        cmd = [
-            cartridge_cmd, version_cmd,
-            "--project-path=invalid_path"
-        ]
+@pytest.mark.parametrize('version_cmd', ['version', '-v', '--version'])
+def test_version_command_invalid_path(cartridge_cmd, version_cmd):
+    cmd = [
+        cartridge_cmd, version_cmd,
+        "--project-path=invalid_path"
+    ]
 
-        rc, output = run_command_and_get_output(cmd)
-        assert rc == 1
-        assert 'Failed to show Cartridge version: Specified project path doesn\'t exist' in output
+    rc, output = run_command_and_get_output(cmd)
+    assert rc == 1
+    assert 'Failed to show Cartridge version: Specified project path doesn\'t exist' in output
