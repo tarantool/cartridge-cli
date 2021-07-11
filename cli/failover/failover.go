7e7a4bf148d232fca4c5b7b4a304a900a6380efb
Alexey Romanov <romanov.alexey2000@gmail.com>
2021-07-12 00:06:56 +0300

    Start failover command

---
+++ cli/failover/failover.go
@@ -0,0 +1,9 @@
+package failover
+
+import "github.com/tarantool/cartridge-cli/cli/context"
+
+// TODO
+func FillCtx(ctx *context.Ctx) error {
+
+	return nil
+}
---
+++ cli/failover/setup.go
@@ -0,0 +1,7 @@
+package failover
+
+import "github.com/tarantool/cartridge-cli/cli/context"
+
+func Setup(ctx *context.Ctx, args []string) error {
+	return nil
+}
