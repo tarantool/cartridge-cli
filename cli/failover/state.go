cb3ab6c219c2bc755aead1f71cf7b25be5699465
Alexey Romanov <romanov.alexey2000@gmail.com>
2021-07-20 01:15:18 +0300

    Add pretty formatting failover state string

--- cli/failover/state.go
+++ cli/failover/state.go
@@ -3,6 +3,7 @@ package failover
 import (
 	"fmt"
 	"reflect"
+	"sort"
 	"strings"
 
 	"github.com/apex/log"
@@ -35,42 +36,96 @@ func State(ctx *context.Ctx) error {
 }
 
 func getFailoverStatePrettyString(resultMap map[string]interface{}) string {
-	if provider, found := resultMap["state_provider"]; found {
-		if provider == "tarantool" {
-			resultMap["state_provider"] = "stateboard"
-			resultMap["stateboard_params"] = resultMap["tarantool_params"]
-			delete(resultMap, "tarantool_params")
-		}
+	switch resultMap["state_provider"] {
+	case "tarantool":
+		resultMap["state_provider"] = "stateboard"
+		resultMap["stateboard_params"] = resultMap["tarantool_params"]
+		delete(resultMap, "tarantool_params")
+		delete(resultMap, "etcd2_params")
+	case "etcd2":
+		delete(resultMap, "tarantool_params")
+	default:
+		panic(project.InternalError("Unknown state provider type: %s", resultMap["state_provider"]))
+	}
+
+	if resultMap["mode"] == "eventual" || resultMap["mode"] == "disabled" {
+		delete(resultMap, "state_provider")
+		delete(resultMap, "etcd2_params")
+		delete(resultMap, "stateboard_params")
 	}
 
 	return internalRecFailoverStatePrettyString(resultMap, 0)
 }
 
-func internalRecFailoverStatePrettyString(result interface{}, tabCnt int) string {
-	acc := ""
+func getSortedFailoverMapKeys(stringMap map[string]interface{}) []string {
+	mapKeys := make([]string, 0, len(stringMap))
+	for key := range stringMap {
+		mapKeys = append(mapKeys, key)
+	}
 
-	for fieldName, value := range result.(map[string]interface{}) {
-		acc = fmt.Sprintf("%s %s• %s:", acc, strings.Repeat("\t", tabCnt), common.ColorHiMagenta.Sprintf(fieldName))
+	// This sorting will allow us to achieve the following format:
+	// * mode
+	// * state_provider
+	// * etcd2_params or stateboard_params
+	// * everything else
+
+	sort.Slice(mapKeys, func(a, b int) bool {
+		if mapKeys[a] == "mode" {
+			return true
+		}
+
+		if mapKeys[b] != "mode" {
+			if mapKeys[a] == "state_provider" {
+				return true
+			}
+
+			return strings.HasSuffix(mapKeys[a], "_params") && mapKeys[b] != "state_provider"
+		}
+
+		return false
+	})
+
+	return mapKeys
+}
+
+func internalRecFailoverStatePrettyString(result interface{}, tabCnt int) string {
+	stateString := ""
+	resultMap := result.(map[string]interface{})
+
+	for _, fieldName := range getSortedFailoverMapKeys(resultMap) {
+		stateString = fmt.Sprintf(
+			"%s %s• %s:", stateString,
+			strings.Repeat("\t", tabCnt), common.ColorCyan.Sprintf(fieldName),
+		)
+
+		value := resultMap[fieldName]
 
 		switch value.(type) {
 		case map[string]interface{}:
-			acc = fmt.Sprintf("%s \n%s", acc, internalRecFailoverStatePrettyString(value, tabCnt+1))
+			stateString = fmt.Sprintf("%s \n%s", stateString, internalRecFailoverStatePrettyString(value, tabCnt+1))
 		case int8:
-			acc = fmt.Sprintf("%s %d\n", acc, value)
+			stateString = fmt.Sprintf("%s %d\n", stateString, value)
 		case bool:
-			acc = fmt.Sprintf("%s %t\n", acc, value)
+			stateString = fmt.Sprintf("%s %t\n", stateString, value)
 		case string:
-			acc = fmt.Sprintf("%s %s\n", acc, value)
+			switch value {
+			case "disabled":
+				stateString = fmt.Sprintf("%s %s\n", stateString, common.ColorRed.Sprintf(value.(string)))
+			case "eventual", "stateful":
+				stateString = fmt.Sprintf("%s %s\n", stateString, common.ColorGreen.Sprintf(value.(string)))
+			default:
+				stateString = fmt.Sprintf("%s %s\n", stateString, value)
+			}
 		case []interface{}:
 			for _, elem := range value.([]interface{}) {
-				acc = fmt.Sprintf("%s %s,", acc, elem)
+				stateString = fmt.Sprintf("%s %s,", stateString, elem)
 			}
 
-			acc = fmt.Sprintf("%s\n", acc[:len(acc)-1])
+			stateString = fmt.Sprintf("%s\n", stateString[:len(stateString)-1])
 		default:
 			panic(project.InternalError("Unknown type: %s", reflect.TypeOf(value)))
 		}
 	}
 
-	return acc
+	return stateString
 }
