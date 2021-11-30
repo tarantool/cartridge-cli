0d36f093499d6ac78643a62ea9092b4b2307392d
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2021-11-30 13:08:26 +0300

    Support Tarantool version as a struct
    Add struct type for Tarantool version and parsing utility. Struct can be
    used to build dependency requirements instead of version string.
    
    Part of #619

--- cli/common/tarantool.go
+++ cli/common/tarantool.go
@@ -14,11 +14,100 @@ import (
 )
 
 var (
+	// Used for shallow validation of a version string.
 	tarantoolVersionRegexp *regexp.Regexp
+	// Used for thorough validation of a version string and to extract string version to a struct.
+	tarantoolVersionFullRegexp *regexp.Regexp
 )
 
 func init() {
 	tarantoolVersionRegexp = regexp.MustCompile(`\d+\.\d+\.\d+[-\w]*`)
+	// Part 1 is semVer X.Y.Z ,
+	// part 2 (optional) is a tag suffix for pre-release,
+	// part 3 is number of commits since tag and commit hash,
+	// part 4 (optional) is enterprise suffix,
+	// part 5 (optional) is development build suffix.
+	tarantoolVersionFullRegexp = regexp.MustCompile(
+		`^(?P<Major>\d+)\.(?P<Minor>\d+)?\.(?P<Patch>\d+)` +
+			`(?:-(?P<TagSuffix>alpha\d+|beta\d+|rc\d+|entrypoint))?` +
+			`-(?P<CommitsSinceTag>\d+)-(?P<CommitHashId>g[0-9a-f]+)` +
+			`(?:-(?P<EnterpriseSDKRevision>r\d+)(?:-(?P<EnterpriseIsOnMacOS>macos))?)?` +
+			`(?:-(?P<IsDevelopmentBuild>dev))?$`)
+}
+
+// findNamedMatches processes regexp with named capture groups
+// and transforms output to a map. If capture group is optional
+// and was not found, map value is empty string.
+func findNamedMatches(regex *regexp.Regexp, str string) map[string]string {
+	match := regex.FindStringSubmatch(str)
+
+	results := map[string]string{}
+	for i, name := range match {
+		// i == 0 is input string.
+		if i != 0 {
+			results[regex.SubexpNames()[i]] = name
+		}
+	}
+	return results
+}
+
+type TarantoolVersion struct {
+	Major                 uint64
+	Minor                 uint64
+	Patch                 uint64
+	TagSuffix             string
+	CommitsSinceTag       uint64
+	CommitHashId          string
+	EnterpriseSDKRevision string
+	EnterpriseIsOnMacOS   bool
+	IsDevelopmentBuild    bool
+}
+
+func atoiUint64(str string) (uint64, error) {
+	res, err := strconv.ParseUint(str, 10, 32)
+	if err != nil {
+		return 0, fmt.Errorf("Failed to parse Tarantool version: cast to number error (%s)", err)
+	}
+	return res, nil
+}
+
+// ParseTarantoolVersion extracts Tarantool version string to a TarantoolVersion struct.
+func ParseTarantoolVersion(version string) (TarantoolVersion, error) {
+	var result TarantoolVersion
+	var err error
+
+	matches := findNamedMatches(tarantoolVersionFullRegexp, version)
+	if len(matches) == 0 {
+		return result, fmt.Errorf("Failed to parse Tarantool version: format is not valid")
+	}
+
+	if result.Major, err = atoiUint64(matches["Major"]); err != nil {
+		return result, err
+	}
+
+	if result.Minor, err = atoiUint64(matches["Minor"]); err != nil {
+		return result, err
+	}
+
+	if result.Patch, err = atoiUint64(matches["Patch"]); err != nil {
+		return result, err
+	}
+
+	result.TagSuffix = matches["TagSuffix"]
+
+	if result.CommitsSinceTag, err = atoiUint64(matches["CommitsSinceTag"]); err != nil {
+		return result, err
+	}
+
+	result.CommitHashId = matches["CommitHashId"]
+
+	result.EnterpriseSDKRevision = matches["EnterpriseSDKRevision"]
+
+	result.EnterpriseIsOnMacOS = (matches["EnterpriseIsOnMacOS"] != "")
+
+	result.IsDevelopmentBuild = (matches["IsDevelopmentBuild"] != "")
+
+	return result, nil
 }
 
 // GetTarantoolDir returns Tarantool executable directory
