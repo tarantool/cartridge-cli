96a3e183ffa0d9715cfacbf2301286fe616658ea
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2023-05-26 11:16:56 +0300

    template: proper test group usage
    Test group entity is passed to each hook/test. Legacy approach is to use
    group as global variable, which should work for simple groups, but is
    invalid for parametrized ones. `cg` variable name was borrowed from
    luatest README (it is likely a "current group" abbreviation).

--- cli/create/templates/cartridge/test/unit/sample_test.lua
+++ cli/create/templates/cartridge/test/unit/sample_test.lua
@@ -2,11 +2,11 @@ local t = require('luatest')
 local g = t.group('unit_sample')
 
 -- create your space here
-g.before_all(function() end)
+g.before_all(function(cg) end) -- luacheck: no unused args
 
 -- drop your space here
-g.after_all(function() end)
+g.after_all(function(cg) end) -- luacheck: no unused args
 
-g.test_sample = function()
+g.test_sample = function(cg) -- luacheck: no unused args
     t.assert_equals(type(box.cfg), 'table')
 end
